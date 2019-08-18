027c5e8d597b6abfceca33dc2e5ae0bf9728191d
Ben Gillbanks <binarymoon@gmail.com>
2019-08-18 22:43:45 +0100

    Add admin settings page

---
+++ admin/index.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Toolbelt Admin Screen.
+ *
+ * @package toolbelt
+ */
+
+/**
+ * Add a "settings" link to Toolbelt in the plugin list.
+ */
+function tb_admin_settings_link( $plugin_actions, $plugin_file ) {
+
+	$new_actions = array();
+
+	if ( basename( TB_PATH ) . '/index.php' === $plugin_file ) {
+
+		$new_actions['sc_settings'] = sprintf(
+			'<a href="%2$s">%1$s</a>',
+			esc_html__( 'Settings', 'toolbelt' ),
+			esc_url( add_query_arg( array( 'page' => 'toolbelt' ), admin_url( 'options-general.php' ) ) )
+		);
+
+	}
+
+	return array_merge( $new_actions, $plugin_actions );
+
+}
+
+add_filter( 'plugin_action_links', 'tb_admin_settings_link', 10, 2 );
+
+
+/**
+ * Add a Toolbelt admin menu item.
+ */
+function tb_admin_menu() {
+
+	add_options_page(
+		'Toolbelt', // Page title.
+		'Toolbelt', // Menu title.
+		'manage_options', // Author capability.
+		'toolbelt', // Slug.
+		'tb_admin_page'
+	);
+
+}
+
+add_action( 'admin_menu', 'tb_admin_menu' );
+
+
+
+
+
+function tb_field( $args ) {
+
+	$key = $args['key'];
+	$options = get_option( 'toolbelt_options' );
+
+	$checked = true;
+	if ( empty( $options[ $key ] ) ) {
+		$checked = false;
+	}
+
+?>
+
+	<tr>
+		<th class="check-column" scope="row">
+			<input
+				id="<?php echo esc_attr( $key ); ?>"
+				name="toolbelt_options[<?php echo esc_attr( $key ); ?>]"
+				<?php checked( $checked ); ?>
+				type="checkbox" />
+		</th>
+		<td class="column-title column-primary">
+			<strong><?php echo esc_html( $args['name'] ); ?></strong>
+		</td>
+		<td>
+			<?php echo esc_html( $args['description'] ); ?>
+		</td>
+	</tr>
+
+<?php
+
+}
+
+
+/**
+ * Display the Toolbelt admin page.
+ */
+function tb_admin_page() {
+
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	tb_save_settings();
+
+	require TB_PATH . 'admin/settings.php';
+
+}
+
+
+/**
+ * Save Toolbelt settings.
+ */
+function tb_save_settings() {
+
+	// Use the nonce to check to see if the form has been submitted.
+	if ( filter_input( INPUT_POST, '_wpnonce' ) === null ) {
+		return;
+	}
+
+	/**
+	 * Check the admin referer.
+	 * Quit automatically if the nonce is missing/ wrong.
+	 */
+	check_admin_referer( 'toolbelt_settings' );
+
+	// Get the options and filter them.
+	$options = filter_input( INPUT_POST, 'toolbelt_options', FILTER_DEFAULT, FILTER_FORCE_ARRAY );
+	// Save the options.
+	update_option( 'toolbelt_options', $options );
+
+}
---
+++ admin/settings.php
@@ -0,0 +1,50 @@
+
+<?php
+/**
+ * Content of the Toolbelt settings page.
+ *
+ * @package toolbelt
+ */
+
+?>
+
+<style>
+	.column-title { width: 20%; }
+</style>
+
+<div class="wrap about-wrap full-width-layout">
+
+	<div class="wp-header-end"></div><!-- admin notices go after .wp-header-end or .wrap>h2:first-child -->
+	<h1><?php esc_html_e( 'Toolbelt settings', 'toolbelt' ); ?></h1>
+
+	<div class="about-text"><?php esc_html_e( 'Toolbelt is a lightweight, privacy focused, collection of things that add commonly needed features to your WordPress website.', 'toolbelt' ); ?></div>
+
+	<form action="" method="POST">
+
+		<?php wp_nonce_field( 'toolbelt_settings' ); ?>
+
+		<table class="wp-list-table widefat fixed striped">
+
+<?php
+
+	$modules = tb_get_modules();
+
+	foreach ( $modules as $slug => $module ) {
+		tb_field(
+			array(
+				'key' => $slug,
+				'name' => $module['name'],
+				'description' => $module['description'],
+			)
+		);
+	}
+
+?>
+
+		</table>
+
+		<?php submit_button( esc_html__( 'Save Settings', 'toolbelt' ) ); ?>
+
+	</form>
+
+</div>
