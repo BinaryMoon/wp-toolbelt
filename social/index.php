24a3fa65d58bca668a684027e32b019462542336
Ben Gillbanks <binarymoon@gmail.com>
2019-08-19 08:36:56 +0100

    Add social sharing feature

---
+++ social/index.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Social sharing links.
+ *
+ * @package toolbelt
+ */
+
+/**
+ * Add social sharing buttons to the post content.
+ */
+function tb_social_sharing( $content ) {
+
+	if ( ! is_singular() ) {
+		return $content;
+	}
+
+	tb_social_styles();
+
+	$html = '';
+
+	// Get the canonical link for the blog post.
+	// Fallback to the permalink.
+	$canonical = wp_get_canonical_url();
+
+	if ( ! $canonical ) {
+		$canonical = get_permalink();
+	}
+
+	// Display a list of social networks.
+	$networks = tb_social_networks();
+
+	foreach ( $networks as $slug => $network ) {
+
+		$url = sprintf( $network['url'], rawurlencode( $canonical ) );
+		$html .= '<a href="' . esc_url( $url ) . '" class="tb_' . esc_attr( $slug ) . '">' . esc_html( $network['label'] ) . '</a>' . "\n";
+
+	}
+
+	return $content . '<section class="tb_social_share">' . $html . '</section>';
+
+}
+
+add_filter( 'the_content', 'tb_social_sharing' );
+
+
+/**
+ * Get a list of social networks and their sharing links.
+ */
+function tb_social_networks() {
+
+	$networks = array(
+		'facebook' => array(
+			'label' => esc_html_x( 'Share this', 'Facebook button label', 'toolbelt' ),
+			'url' => 'https://facebook.com/sharer/sharer.php?u=%s',
+		),
+		'twitter' => array(
+			'label' => esc_html_x( 'Tweet this', 'Twitter button label', 'toolbelt' ),
+			'url' => 'https://twitter.com/intent/tweet?url=%s',
+		),
+		'linkedin' => array(
+			'label' => esc_html_x( 'Share this', 'LinkedIn button label', 'toolbelt' ),
+			'url' => 'https://www.linkedin.com/shareArticle?mini=true&url=%s',
+		),
+		'whatsapp' => array(
+			'label' => esc_html_x( 'Share this', 'WhatsApp button label', 'toolbelt' ),
+			'url' => 'https://wa.me/?text=%s',
+		),
+		'pinterest' => array(
+			'label' => esc_html_x( 'Pin this', 'Pinterest button label', 'toolbelt' ),
+			'url' => 'https://pinterest.com/pin/create/button/?url=%s',
+		),
+	);
+
+	return apply_filters( 'tb_social_networks', $networks );
+
+}
+
+
+/**
+ * Display the social styles.
+ */
+function tb_social_styles() {
+
+	$path = plugin_dir_path( __FILE__ );
+
+	// Output bar styles. Do this first so that the bar has styles instantly.
+	echo '<style>';
+	require $path . 'style.min.css';
+	echo '</style>';
+
+}
