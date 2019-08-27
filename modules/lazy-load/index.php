e951387a371e2b781f619900f9298b96c10faf46
Ben Gillbanks <binarymoon@gmail.com>
2019-08-27 22:33:23 +0100

    Add lazy loading for iframes and avatars

--- modules/lazy-load/index.php
+++ modules/lazy-load/index.php
@@ -6,12 +6,12 @@
  */
 
 /**
- * Add 'loading="lazy" to all images in 'the_content'.
+ * Add 'loading="lazy" to images.
  *
- * @param string $content The post content.
+ * @param string $content The content to check.
  * @return string
  */
-function toolbelt_lazy_load( $content ) {
+function toolbelt_lazy_load_image( $content ) {
 
 	return preg_replace(
 		'/<img /',
@@ -21,4 +21,23 @@ function toolbelt_lazy_load( $content ) {
 
 }
 
-add_filter( 'the_content', 'toolbelt_lazy_load', 100 );
+
+/**
+ * Add 'loading="lazy" to iframes.
+ *
+ * @param string $content The content to check.
+ * @return string
+ */
+function toolbelt_lazy_load_iframe( $content ) {
+
+	return preg_replace(
+		'/<iframe /',
+		'<iframe loading="lazy" ',
+		$content
+	);
+
+}
+
+add_filter( 'the_content', 'toolbelt_lazy_load_image', 100 );
+add_filter( 'the_content', 'toolbelt_lazy_load_iframe', 100 );
+add_filter( 'get_avatar', 'toolbelt_lazy_load_image', 100 );
