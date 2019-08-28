6c303a449a9c522143e10774e2a504d3bee47296
Ben Gillbanks <binarymoon@gmail.com>
2019-08-28 10:14:45 +0100

    fix translatable text

--- index.php
+++ index.php
@@ -50,53 +50,53 @@ function toolbelt_get_modules() {
 	return array(
 		'cookie-banner' => array(
 			'name' => esc_html__( 'EU Cookie Banner', 'wp-toolbelt' ),
-			'description' => esc_html( 'Display a simple banner with a link to your Privacy Policy.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'Display a simple banner with a link to your Privacy Policy.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Cookie-Banner',
 			'weight' => esc_html__( '1.2kb of inline JS and CSS.', 'wp-toolbelt' ),
 		),
 		'social-sharing' => array(
 			'name' => esc_html__( 'Static Social Sharing', 'wp-toolbelt' ),
-			'description' => esc_html( 'Add social sharing links that use the platforms native sharing system.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'Add social sharing links that use the platforms native sharing system.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Static-Social-Sharing',
 			'weight' => esc_html__( '4.1kb of inline SVG icons, and 0.7kb of inline CSS.', 'wp-toolbelt' ),
 		),
 		'projects' => array(
 			'name' => esc_html__( 'Portfolio', 'wp-toolbelt' ),
-			'description' => esc_html( 'A portfolio custom post type.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'A portfolio custom post type.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Portfolio',
 		),
 		'cleanup' => array(
 			'name' => esc_html__( 'Header Cleanup', 'wp-toolbelt' ),
-			'description' => esc_html( 'Remove unnecessary HTML from the site header.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'Remove unnecessary HTML from the site header.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Optimization',
 			'weight' => esc_html__( 'Minus 5kb of HTML from the page head.', 'wp-toolbelt' ),
 		),
 		'breadcrumbs' => array(
 			'name' => esc_html__( 'Breadcrumbs', 'wp-toolbelt' ),
-			'description' => esc_html( 'Simple, fast, breadcrumbs. Requires support from the theme to display. See docs for more info.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'Simple, fast, breadcrumbs. Requires support from the theme to display. See docs for more info.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Breadcrumbs',
 			'weight' => esc_html__( '1 or 2kb of HTML.', 'wp-toolbelt' ),
 		),
 		'related-posts' => array(
 			'name' => esc_html__( 'Related Posts', 'wp-toolbelt' ),
-			'description' => esc_html( 'Speedy related posts.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'Speedy related posts.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Related-Posts',
 			'weight' => esc_html__( '0.3kb of inline CSS, plus the HTML and images.', 'wp-toolbelt' ),
 		),
 		'lazy-load' => array(
 			'name' => esc_html__( 'Lazy Load images', 'wp-toolbelt' ),
-			'description' => esc_html( 'Add native browser lazy loading to all images on your website. Currently this only works in Chrome, but hopefully it will be added to all browsers.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'Add native browser lazy loading to all images on your website. Currently this only works in Chrome, but hopefully it will be added to all browsers.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Lazy-Loading',
 		),
 		'social-menu' => array(
 			'name' => esc_html__( 'Social Menu', 'wp-toolbelt' ),
-			'description' => esc_html( 'Add a social icons menu. This must be integrated into the theme.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'Add a social icons menu. This must be integrated into the theme.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Social-Menu',
 			'weight' => esc_html__( '0.2kb of inline CSS, plus the SVGs needed for the icons' ),
 		),
 		'featured-attachment' => array(
 			'name' => esc_html__( 'Featured Attachment', 'wp-toolbelt' ),
-			'description' => esc_html( 'If there is no featured image for a post then use the first image attachment instead.', 'wp-toolbelt' ),
+			'description' => esc_html__( 'If there is no featured image for a post then use the first image attachment instead.', 'wp-toolbelt' ),
 			'docs' => 'https://github.com/BinaryMoon/wp-toolbelt/wiki/Featured-Attachment',
 		),
 	);
