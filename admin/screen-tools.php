a8605be11a7d580074d234a169ac17015db56183
Ben Gillbanks <binarymoon@gmail.com>
2019-09-17 09:25:52 +0100

    Tweak tools screen styles

--- admin/screen-tools.php
+++ admin/screen-tools.php
@@ -7,7 +7,17 @@
 
 ?>
 
-<div class="wrap">
+<style>
+.about-wrap h2 {
+	text-align: left;
+}
+p.submit {
+	margin-top: 0;
+	padding: 0;
+}
+</style>
+
+<div class="wrap about-wrap full-width-layout">
 
 	<div class="wp-header-end"></div><!-- admin notices go after .wp-header-end or .wrap>h2:first-child -->
 	<h1><?php esc_html_e( 'Toolbelt Tools', 'wp-toolbelt' ); ?></h1>
