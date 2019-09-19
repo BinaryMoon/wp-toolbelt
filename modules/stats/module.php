7305078903a785673deba0eb7ab2a77b39d0606f
Ben Gillbanks <binarymoon@gmail.com>
2019-09-19 20:49:01 +0100

    Add a link in the admin bar to the stats page

--- modules/stats/module.php
+++ modules/stats/module.php
@@ -24,3 +24,38 @@ function toolbelt_stats() {
 }
 
 toolbelt_stats();
+
+
+/**
+ * Add a link in the Admin Bar to the stats dashboard.
+ *
+ * In the future I would like to add a bar graph to the admin bar like the one
+ * in Jetpack.
+ *
+ * @param WP_Admin_Bar $wp_admin_bar A reference to the admin bar object.
+ * @return void
+ */
+function toolbelt_stats_toolbar_link( $wp_admin_bar ) {
+
+	$settings = get_option( 'toolbelt_settings' );
+
+	// Quit if no dashboard has been specified.
+	if ( empty( $settings['stats-dashboard-url'] ) ) {
+		return;
+	}
+
+	$args = array(
+		'id' => 'toolbelt-stats',
+		'title' => esc_html__( 'Site Stats', 'wp-toolbelt' ),
+		'href' => esc_url( $settings['stats-dashboard-url'] ),
+		'meta' => array(
+			'class' => 'toolbelt-stats',
+			'target' => '_blank',
+		),
+	);
+
+	$wp_admin_bar->add_node( $args );
+
+}
+
+add_action( 'admin_bar_menu', 'toolbelt_stats_toolbar_link', 999 );
