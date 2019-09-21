2287a717f8c1e7402b3984ebe59e458cbd12c778
Ben Gillbanks <binarymoon@gmail.com>
2019-09-21 20:34:01 +0100

    add additional save modules button to modules settings screen

--- admin/screen-modules.php
+++ admin/screen-modules.php
@@ -16,7 +16,7 @@ do_action( 'toolbelt_module_settings' );
 	.column-title { width: 20%; }
 	.column-weight { width: 25%; }
 	p.search-box { float: none; }
-	.search-box input[name="s"] { width: 100%; font-size: 18px; height: 40px; margin-bottom: 15px; }
+	.search-box input[name="s"] { width: 100%; font-size: 18px; height: 40px; margin-bottom: 30px; }
 	td p.doc-link { margin-bottom: 0; }
 	td p.doc-link a + a { margin: 0 0.5em; }
 	.wrap { max-width: 600px; margin: 0 auto; }
@@ -67,6 +67,7 @@ function toolbeltFilterModules() {
 		</p>
 
 		<?php wp_nonce_field( 'toolbelt_modules' ); ?>
+		<?php submit_button( esc_html__( 'Save Modules', 'wp-toolbelt' ) ); ?>
 
 		<table class="wp-list-table widefat fixed striped" id="toolbelt-modules-table">
 			<thead>
@@ -101,7 +102,7 @@ function toolbeltFilterModules() {
 
 		</table>
 
-		<?php submit_button( esc_html__( 'Save Settings', 'wp-toolbelt' ) ); ?>
+		<?php submit_button( esc_html__( 'Save Modules', 'wp-toolbelt' ) ); ?>
 
 	</form>
 
