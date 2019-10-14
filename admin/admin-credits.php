f20ff210bd37d0f8e6f43f4cadaf17c0414feabf
Ben Gillbanks <binarymoon@gmail.com>
2019-10-14 12:37:42 +0100

    tweak the toolbelt ratings box design

--- admin/admin-credits.php
+++ admin/admin-credits.php
@@ -8,8 +8,13 @@
 ?>
 
 <style>
-	.toolbelt-ratings { margin: 30px 0; }
-	.toolbelt-ratings p { font-size: 16px; }
+	.toolbelt-ratings {
+		margin: 30px 0;
+		padding: 1px 15px;
+		background: rgba( 255,255,200, 0.5 );
+		border-radius: 5px;
+	}
+	.toolbelt-ratings p { font-size: 14px; }
 </style>
 
 <div class="toolbelt-ratings">
