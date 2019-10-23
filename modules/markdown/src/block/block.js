23765c02044854f9ac9a34d2ae29f98f9ddefef2
Ben Gillbanks <binarymoon@gmail.com>
2019-10-23 22:32:17 +0100

    wrap blocks in self executing function
    and fix the serversiderender deprecation

--- modules/markdown/src/block/block.js
+++ modules/markdown/src/block/block.js
@@ -1,144 +1,148 @@
-const { registerBlockType } = wp.blocks;
-const { createElement, Fragment, Component, RawHTML } = wp.element;
-const { ExternalLink, Path, Rect, SVG } = wp.components;
-const { __ } = wp.i18n;
-const { PlainText } = wp.blockEditor;
+( function() {
 
-const exampleTitle = __( 'Try Markdown', 'wp-toolbelt' );
-const exampleDescription = __(
-	'Markdown is a text formatting syntax that is converted into HTML. You can _emphasize_ text or **make it strong** with just a few characters.',
-	'wp-toolbelt'
-);
+	const { registerBlockType } = wp.blocks;
+	const { createElement, Fragment, Component, RawHTML } = wp.element;
+	const { ExternalLink, Path, Rect, SVG } = wp.components;
+	const { __ } = wp.i18n;
+	const { PlainText } = wp.blockEditor;
 
-registerBlockType(
-	'toolbelt/markdown',
-	{
+	const exampleTitle = __( 'Try Markdown', 'wp-toolbelt' );
+	const exampleDescription = __(
+		'Markdown is a text formatting syntax that is converted into HTML. You can _emphasize_ text or **make it strong** with just a few characters.',
+		'wp-toolbelt'
+	);
 
-		title: __( 'Markdown', 'wp-toolbelt' ),
+	registerBlockType(
+		'toolbelt/markdown',
+		{
 
-		description: (
-			<Fragment>
-				<p>
-					{__( 'Use regular characters and punctuation to style text, links, and lists.', 'wp-toolbelt' )}
-				</p>
-				<ExternalLink href="https://en.support.wordpress.com/markdown-quick-reference/">
-					{__( 'Support reference', 'wp-toolbelt' )}
-				</ExternalLink>
-			</Fragment>
-		),
+			title: __( 'Markdown', 'wp-toolbelt' ),
 
-		keywords: [ __( 'toolbelt', 'wp-toolbelt' ) ],
-
-		icon: {
-			src: (
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 208 128">
-					<Rect
-						width="198"
-						height="118"
-						x="5"
-						y="5"
-						ry="10"
-						stroke="currentColor"
-						strokeWidth="10"
-						fill="none"
-					/>
-					<Path d="M30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zM155 98l-30-33h20v-35h20v35h20z" />
-				</svg>
-			)
-		},
-
-		category: 'common',
-
-		attributes: {
-			source: {
-				type: 'string',
-			},
-		},
-
-		supports: {
-			html: false,
-		},
-
-		/**
-		 * Save the formatted markdown content.
-		 */
-		save( props ) {
-
-			const { attributes, className } = props;
-			const { source } = attributes;
-
-			return (
-				<RawHTML className={className}>
-					{source.length ? marked( source ) : ''}
-				</RawHTML>
-			);
-
-		},
-
-		/**
-		 * Edit the settings.
-		 */
-		edit( props ) {
-
-			const { attributes, isSelected, className } = props;
-			const { source } = attributes;
-
-			/**
-			 * Check to see if the markdown content is empty or not.
-			 */
-			const isEmpty = () => {
-				return !source || source.trim() === '';
-			}
-
-			/**
-			 * Upadte the markdown content attribute.
-			 */
-			const updateSource = ( source ) => {
-				props.setAttributes( { source } );
-			}
-
-			/**
-			 * Display a placeholder.
-			 */
-			if ( !isSelected && isEmpty() ) {
-				return (
-					<p className={className}>
-						<strong>{__( 'Write your _Markdown_ **here**…', 'wp-toolbelt' )}</strong>
+			description: (
+				<Fragment>
+					<p>
+						{__( 'Use regular characters and punctuation to style text, links, and lists.', 'wp-toolbelt' )}
 					</p>
-				);
-			}
+					<ExternalLink href="https://en.support.wordpress.com/markdown-quick-reference/">
+						{__( 'Support reference', 'wp-toolbelt' )}
+					</ExternalLink>
+				</Fragment>
+			),
+
+			keywords: [ __( 'toolbelt', 'wp-toolbelt' ) ],
+
+			icon: {
+				src: (
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 208 128">
+						<Rect
+							width="198"
+							height="118"
+							x="5"
+							y="5"
+							ry="10"
+							stroke="currentColor"
+							strokeWidth="10"
+							fill="none"
+						/>
+						<Path d="M30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zM155 98l-30-33h20v-35h20v35h20z" />
+					</svg>
+				)
+			},
+
+			category: 'common',
+
+			attributes: {
+				source: {
+					type: 'string',
+				},
+			},
+
+			supports: {
+				html: false,
+			},
 
 			/**
-			 * Render the markdown content.
+			 * Save the formatted markdown content.
 			 */
-			if ( !isSelected && !isEmpty() ) {
+			save( props ) {
+
+				const { attributes, className } = props;
+				const { source } = attributes;
+
 				return (
 					<RawHTML className={className}>
 						{source.length ? marked( source ) : ''}
 					</RawHTML>
-				)
-			}
+				);
+
+			},
 
 			/**
-			 * Edit the markdown content.
+			 * Edit the settings.
 			 */
-			return (
-				<div className={className}>
-					<PlainText
-						placeholder={__( 'Write your _Markdown_ **here**…', 'wp-toolbelt' )}
-						value={source}
-						onChange={updateSource}
-					></PlainText >
-				</div>
-			);
+			edit( props ) {
 
-		},
+				const { attributes, isSelected, className } = props;
+				const { source } = attributes;
 
-		example: {
-			attributes: {
-				source: `## ## ${exampleTitle}\n\n${exampleDescription}`,
+				/**
+				 * Check to see if the markdown content is empty or not.
+				 */
+				const isEmpty = () => {
+					return !source || source.trim() === '';
+				}
+
+				/**
+				 * Upadte the markdown content attribute.
+				 */
+				const updateSource = ( source ) => {
+					props.setAttributes( { source } );
+				}
+
+				/**
+				 * Display a placeholder.
+				 */
+				if ( !isSelected && isEmpty() ) {
+					return (
+						<p className={className}>
+							<strong>{__( 'Write your _Markdown_ **here**…', 'wp-toolbelt' )}</strong>
+						</p>
+					);
+				}
+
+				/**
+				 * Render the markdown content.
+				 */
+				if ( !isSelected && !isEmpty() ) {
+					return (
+						<RawHTML className={className}>
+							{source.length ? marked( source ) : ''}
+						</RawHTML>
+					)
+				}
+
+				/**
+				 * Edit the markdown content.
+				 */
+				return (
+					<div className={className}>
+						<PlainText
+							placeholder={__( 'Write your _Markdown_ **here**…', 'wp-toolbelt' )}
+							value={source}
+							onChange={updateSource}
+						></PlainText >
+					</div>
+				);
+
 			},
-		}
 
-	}
-);
+			example: {
+				attributes: {
+					source: `## ## ${exampleTitle}\n\n${exampleDescription}`,
+				},
+			}
+
+		}
+	);
+
+} )();
--- modules/testimonials/src/block/block.js
+++ modules/testimonials/src/block/block.js
@@ -1,107 +1,111 @@
-const { registerBlockType } = wp.blocks;
-const { createElement } = wp.element;
-const { InspectorControls } = wp.blockEditor;
-const { RangeControl, RadioControl, PanelBody, ServerSideRender } = wp.components;
-// const { ServerSideRender } = wp.serverSideRender;
-const { __ } = wp.i18n;
+( function() {
 
-registerBlockType(
-	'toolbelt/testimonials',
-	{
-		title: __( 'Testimonials', 'wp-toolbelt' ),
+	const { registerBlockType } = wp.blocks;
+	const { createElement } = wp.element;
+	const { InspectorControls } = wp.blockEditor;
+	const { RangeControl, RadioControl, PanelBody } = wp.components;
+	const ServerSideRender = wp.serverSideRender;
+	const { __ } = wp.i18n;
 
-		icon: 'testimonial',
+	registerBlockType(
+		'toolbelt/testimonials',
+		{
+			title: __( 'Testimonials', 'wp-toolbelt' ),
 
-		description: __( 'Display a grid of Toolbelt Testimonials.', 'wp-toolbelt' ),
+			icon: 'testimonial',
 
-		keywords: [ __( 'toolbelt', 'wp-toolbelt' ) ],
+			description: __( 'Display a grid of Toolbelt Testimonials.', 'wp-toolbelt' ),
 
-		category: 'common',
+			keywords: [ __( 'toolbelt', 'wp-toolbelt' ) ],
 
-		supports: {
-			align: [ 'full', 'wide' ],
-		},
+			category: 'common',
 
-		attributes: {
-			rows: {
-				default: 2,
+			supports: {
+				align: [ 'full', 'wide' ],
 			},
-			columns: {
-				default: 2
+
+			attributes: {
+				rows: {
+					default: 2,
+				},
+				columns: {
+					default: 2
+				},
+				orderby: {
+					default: 'date'
+				}
 			},
-			orderby: {
-				default: 'date'
+
+			edit( props ) {
+
+				const attributes = props.attributes;
+				const setAttributes = props.setAttributes;
+
+				// Function to update the number of rows.
+				const changeRows = ( rows ) => {
+					setAttributes( { rows } );
+				}
+
+				// Function to update the number of columns.
+				const changeColumns = ( columns ) => {
+					setAttributes( { columns } );
+				}
+
+				// Function to update the testimonial order.
+				const changeOrderby = ( orderby ) => {
+					setAttributes( { orderby } );
+				}
+
+				return [
+					<ServerSideRender
+						block="toolbelt/testimonials"
+						attributes={attributes}
+					/>,
+					<InspectorControls>
+						<PanelBody
+							title={__( 'Layout', 'wp-toolbelt' )}
+							initialOpen={true}
+						>
+
+							<RangeControl
+								value={attributes.rows}
+								label={__( 'Rows', 'wp-toolbelt' )}
+								onChange={changeRows}
+								min={1}
+								max={10}
+							/>
+
+							<RangeControl
+								value={attributes.columns}
+								label={__( 'Columns', 'wp-toolbelt' )}
+								onChange={changeColumns}
+								min={1}
+								max={4}
+							/>
+
+							<RadioControl
+								selected={attributes.orderby}
+								label={__( 'Order by', 'wp-toolbelt' )}
+								onChange={changeOrderby}
+								options={
+									[
+										{ value: 'date', label: __( 'date', 'wp-toolbelt' ) },
+										{ value: 'rand', label: __( 'random', 'wp-toolbelt' ) },
+									]
+								}
+							/>
+
+						</PanelBody>
+					</InspectorControls>
+				];
+
+			},
+
+			save() {
+				return null;
 			}
-		},
 
-		edit( props ) {
-
-			const attributes = props.attributes;
-			const setAttributes = props.setAttributes;
-
-			// Function to update the number of rows.
-			const changeRows = ( rows ) => {
-				setAttributes( { rows } );
-			}
-
-			// Function to update the number of columns.
-			const changeColumns = ( columns ) => {
-				setAttributes( { columns } );
-			}
-
-			// Function to update the testimonial order.
-			const changeOrderby = ( orderby ) => {
-				setAttributes( { orderby } );
-			}
-
-			return [
-				<ServerSideRender
-					block="toolbelt/testimonials"
-					attributes={attributes}
-				/>,
-				<InspectorControls>
-					<PanelBody
-						title={__( 'Layout', 'wp-toolbelt' )}
-						initialOpen={true}
-					>
-
-						<RangeControl
-							value={attributes.rows}
-							label={__( 'Rows', 'wp-toolbelt' )}
-							onChange={changeRows}
-							min={1}
-							max={10}
-						/>
-
-						<RangeControl
-							value={attributes.columns}
-							label={__( 'Columns', 'wp-toolbelt' )}
-							onChange={changeColumns}
-							min={1}
-							max={4}
-						/>
-
-						<RadioControl
-							selected={attributes.orderby}
-							label={__( 'Order by', 'wp-toolbelt' )}
-							onChange={changeOrderby}
-							options={
-								[
-									{ value: 'date', label: __( 'date', 'wp-toolbelt' ) },
-									{ value: 'rand', label: __( 'random', 'wp-toolbelt' ) },
-								]
-							}
-						/>
-
-					</PanelBody>
-				</InspectorControls>
-			];
-
-		},
-
-		save() {
-			return null;
 		}
+	);
 
-	}
-);
+} )();
