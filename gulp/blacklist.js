f31014cb9a325ac1ff556830bf72f7867d9414db
Ben Gillbanks <binarymoon@gmail.com>
2019-10-27 08:51:40 +0000

    Update blacklist.js

--- gulp/blacklist.js
+++ gulp/blacklist.js
@@ -7,6 +7,7 @@ const download = require( 'gulp-download' );
 export default function update_blacklist() {
 
 	const blacklist_url = 'https://raw.githubusercontent.com/splorp/wordpress-comment-blacklist/master/blacklist.txt';
+
 	return download( blacklist_url )
 		.pipe( gulp.dest( './modules/spam-blocker/' ) );
 
