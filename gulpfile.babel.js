9396d1144b78fd5edc373e5743e871da4c428910
Ben Gillbanks <binarymoon@gmail.com>
2019-11-07 23:11:15 +0000

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,7 +17,8 @@ import {
 	styles_breadcrumbs, styles_videos,
 	styles_heading_links, styles_infinite_scroll,
 	styles_testimonials, styles_admin_tweaks,
-	styles_portfolio
+	styles_portfolio, styles_block_contact,
+	styles_contact
 } from './gulp/sass';
 
 import {
@@ -29,7 +30,8 @@ import {
 import {
 	block_markdown,
 	block_testimonials,
-	block_projects
+	block_projects,
+	block_contact_form
 } from './gulp/script-block';
 
 import update_blacklist from './gulp/blacklist';
@@ -49,6 +51,7 @@ import {
 export const buildTranslations = translate;
 export const buildZip = compress;
 export const buildBlacklist = update_blacklist;
+export const buildContact = block_contact_form;
 export const buildStats = parallel( jetpack_stats, toolbelt_stats );
 
 export const build = series(
@@ -64,6 +67,8 @@ export const build = series(
 		styles_admin_tweaks,
 		styles_portfolio,
 		styles_testimonials,
+		styles_block_contact,
+		styles_contact,
 		scripts_cookieBanner,
 		scripts_infiniteScroll,
 		scripts_spam,
@@ -71,6 +76,7 @@ export const build = series(
 		block_testimonials,
 		block_projects,
 		block_markdown,
+		block_contact_form,
 		translate,
 		update_blacklist
 	),
@@ -83,6 +89,7 @@ export const watchFiles = function( done ) {
 		'./modules/*/src/sass/*.scss',
 		parallel(
 			styles_cookie,
+			styles_contact,
 			styles_social,
 			styles_related_posts,
 			styles_breadcrumbs,
@@ -93,6 +100,13 @@ export const watchFiles = function( done ) {
 			styles_admin_tweaks,
 			styles_testimonials,
 			styles_portfolio
+		)
+	);
+
+	watch(
+		'./modules/*/src/block/*.scss',
+		parallel(
+			styles_block_contact
 		)
 	);
 
@@ -114,7 +128,8 @@ export const watchFiles = function( done ) {
 			parallel(
 				block_testimonials,
 				block_projects,
-				block_markdown
+				block_markdown,
+				block_contact_form
 			)
 		)
 	);
