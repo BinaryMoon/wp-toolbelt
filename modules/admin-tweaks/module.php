e7b6ba791c7d5b864ff6372d9309e7ebce53a7ab
Ben Gillbanks <binarymoon@gmail.com>
2019-11-26 23:32:46 +0000

    make the admin tweaks display in the customizer

--- modules/admin-tweaks/module.php
+++ modules/admin-tweaks/module.php
@@ -5,22 +5,18 @@
  * @package toolbelt
  */
 
-if ( ! is_admin() ) {
-	return;
-}
-
-
 /**
  * Display some custom css to tweak the WordPress admin.
  */
 function toolbelt_admin_tweaks() {
 
-	if ( is_admin() ) {
-
-		toolbelt_styles( 'admin-tweaks' );
-
+	if ( ! is_admin() ) {
+		return;
 	}
+
+	toolbelt_styles( 'admin-tweaks' );
 
 }
 
 add_action( 'admin_head', 'toolbelt_admin_tweaks' );
+add_action( 'customize_controls_print_styles', 'toolbelt_admin_tweaks' );
