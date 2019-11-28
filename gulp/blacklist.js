1e57765aef33266333a036957bc6af8cfd2051f2
Ben Gillbanks <binarymoon@gmail.com>
2019-11-28 21:22:50 +0000

    prevent gulp crashing when there's no web connection

--- gulp/blacklist.js
+++ gulp/blacklist.js
@@ -3,12 +3,14 @@
 
 const gulp = require( 'gulp' );
 const download = require( 'gulp-download' );
+const plumber = require( 'gulp-plumber' );
 
 export default function update_blacklist() {
 
 	const blacklist_url = 'https://raw.githubusercontent.com/splorp/wordpress-comment-blacklist/master/blacklist.txt';
 
 	return download( blacklist_url )
+		.pipe( plumber() )
 		.pipe( gulp.dest( './modules/spam-blocker/' ) );
 
 }
