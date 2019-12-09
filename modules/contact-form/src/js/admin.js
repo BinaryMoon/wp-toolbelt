59dbf1671107ce9cfe97ab2e09bbe8297af6d2ad
Ben Gillbanks <binarymoon@gmail.com>
2019-12-09 10:26:19 +0000

    add toggle button js and simplify the ajax requests

--- modules/contact-form/src/js/admin.js
+++ modules/contact-form/src/js/admin.js
@@ -2,11 +2,63 @@
 jQuery( document ).ready(
 	function( $ ) {
 
+		/**
+		 * Callback method for row status change.
+		 *
+		 * Fades out then deletes the affected row and updates the status
+		 * navigation.
+		 */
+		const rowCallback = function( response ) {
+
+			response = JSON.parse( response );
+			const id = '#post-' + response.id;
+
+			$( id )
+				.css( { backgroundColor: '#FF7979' } )
+				.fadeOut(
+					350,
+					function() {
+						$( this ).remove();
+						$( 'ul.subsubsub' ).html( response.html );
+					}
+				);
+
+		};
+
+		/**
+		 * The desired minimum height for the messages.
+		 */
+		const desired_height = 150;
+
+		/**
+		 * The expand and contract button click event.
+		 */
+		const buttonClickEvent = function( e ) {
+
+			e.preventDefault();
+
+			const $this = $( this );
+			const $parent = $this.prev( '.toolbelt-excerpt' );
+
+			const height = parseInt( $parent.outerHeight() );
+
+			if ( height > desired_height ) {
+				$parent.css( { height: desired_height + '.px' } );
+				$this.html( toolbelt_cpt_actions.expand_button );
+			} else {
+				$parent.css( { 'height': 'auto' } );
+				$this.html( toolbelt_cpt_actions.collapse_button );
+			}
+
+		};
+
+		/**
+		 * Mark a post as not spam.
+		 */
 		$( '.feedback-ham a' ).click(
 			function( e ) {
 
 				e.preventDefault();
-
 				const id = $( this ).data( 'id' );
 
 				$.post(
@@ -16,28 +68,18 @@ jQuery( document ).ready(
 						post_id: id,
 						_ajax_nonce: 3//'<?php echo wp_create_nonce( 'grunion- post - status - ' . $post_id ); ?>'
 					},
-					function( r ) {
-
-						$( '#post-' + id )
-							.css( { backgroundColor: '#59C859' } )
-							.fadeOut(
-								350,
-								() => {
-									$( this ).remove();
-									$( 'ul.subsubsub' ).html( r );
-								}
-							);
-
-					}
+					rowCallback
 				);
 			}
 		);
 
+		/**
+		 * Mark a post as spam.
+		 */
 		$( '.feedback-spam a' ).click(
 			function( e ) {
 
 				e.preventDefault();
-
 				const id = $( this ).data( 'id' );
 
 				$.post(
@@ -47,18 +89,40 @@ jQuery( document ).ready(
 						post_id: id,
 						_ajax_nonce: 3//'<?php echo wp_create_nonce( 'grunion- post - status - ' . $post_id ); ?>'
 					},
-					function( r ) {
-						$( '#post-' + id )
-							.css( { backgroundColor: '#FF7979' } )
-							.fadeOut(
-								350,
-								() => {
-									$( this ).remove();
-									$( 'ul.subsubsub' ).html( r );
-								}
-							);
-					}
+					rowCallback
 				);
+			}
+		);
+
+		/**
+		 * Loop through all of the post excerpts and add a toggle button that
+		 * shows and hides the content.
+		 */
+		$( '.toolbelt-excerpt' ).each(
+			function() {
+
+				const $this = $( this );
+				const height = parseInt( $this.outerHeight() );
+
+				// Only add the toggle button if the content is long enough.
+				if ( height > desired_height ) {
+
+					$this.css(
+						{
+							'height': desired_height + 'px',
+							'overflow': 'hidden',
+						}
+					);
+
+					// Add the toggle button.
+					const $button = $this.next( 'button' );
+					$button
+						.html( toolbelt_cpt_actions.expand_button )
+						.on( 'click', buttonClickEvent )
+						.show();
+
+				}
+
 			}
 		);
 
