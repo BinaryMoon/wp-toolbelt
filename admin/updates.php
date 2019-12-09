a4fd037eafd9fe9c93a3e11de014ff18c6005665
Ben Gillbanks <binarymoon@gmail.com>
2019-12-09 22:26:56 +0000

    clean relate post transients on plugin update as well

--- admin/updates.php
+++ admin/updates.php
@@ -6,15 +6,21 @@
  */
 
 /**
- * Delete autoloading transients.
+ * Delete autoloading related posts transients.
  *
  * @return void
  */
 function toolbelt_related_posts_clear_transients() {
 
 	global $wpdb;
-	$wpdb->query( "DELETE FROM `$wpdb->options` WHERE `option_name` LIKE ('_transient_toolbelt_related_post_%') AND autoload = 'yes'" );
+
+	$wpdb->query(
+		"DELETE FROM `$wpdb->options`
+			WHERE `option_name`
+			LIKE ('_transient_toolbelt_related_post_%') AND autoload = 'yes'"
+	);
 
 }
 
 add_action( 'toolbelt_module_settings', 'toolbelt_related_posts_clear_transients' );
+add_action( 'upgrader_process_complete', 'toolbelt_related_posts_clear_transients' );
