6218c971b333c7e66717302ef7f5c2150517b517
Ben Gillbanks <binarymoon@gmail.com>
2020-02-10 11:21:15 +0000

    fix phpstan errors

--- modules/search-redirect/module.php
+++ modules/search-redirect/module.php
@@ -8,6 +8,8 @@
 /**
  * If there is only one search result then redirect the page to the resultant
  * page.
+ *
+ * @return void
  */
 function toolbelt_search_single_result() {
 
@@ -15,8 +17,14 @@ function toolbelt_search_single_result() {
 
 		global $wp_query;
 		if ( 1 === $wp_query->post_count && 1 === $wp_query->max_num_pages ) {
-			wp_safe_redirect( get_permalink( $wp_query->posts[0]->ID ) );
-			exit;
+
+			$permalink = get_permalink( $wp_query->posts[0]->ID );
+
+			// Ensure there's actually somewhere to redirect to.
+			if ( $permalink ) {
+				wp_safe_redirect( $permalink );
+				exit;
+			}
 		}
 
 	}
