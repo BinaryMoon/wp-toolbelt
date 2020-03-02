994af25e914df33bee7d48b4ca5d6c4a977fb8c3
Ben Gillbanks <binarymoon@gmail.com>
2020-03-02 10:14:47 +0000

    simplify cronjob setup

--- admin/updates.php
+++ admin/updates.php
@@ -33,30 +33,60 @@ add_action( 'upgrader_process_complete', 'toolbelt_related_posts_clear_transient
  */
 function toolbelt_contact_plugin_de_activation() {
 
-	$options = toolbelt_get_options();
-	$cron_key = 'toolbelt_cron_daily';
+	/**
+	 * Deactivate all cron jobs.
+	 */
+	toolbelt_deactivate_cron();
 
-	$module_active = false;
-	if ( isset( $options['contact-form'] ) ) {
-		$module_active = ( 'on' === $options['contact-form'] );
-	}
-
-	// Enable the cron job if it hasn't been set up already.
-	if ( $module_active ) {
-
-		if ( ! wp_next_scheduled( $cron_key ) ) {
-			wp_schedule_event( time(), 'daily', $cron_key );
-		}
-	}
-
-	// Disable the cron job if it is currently running.
-	if ( ! $module_active ) {
-
-		wp_clear_scheduled_hook( $cron_key );
-
-	}
+	/**
+	 * Enable crons for specific modules.
+	 *
+	 * Modules can share cron jobs. Just use the same duration.
+	 */
+	toolbelt_enable_cron( 'contact-form', 'daily' );
+	toolbelt_enable_cron( 'spam-blocker', 'weekly' );
 
 }
 
 add_action( 'toolbelt_module_settings', 'toolbelt_contact_plugin_de_activation' );
 add_action( 'upgrader_process_complete', 'toolbelt_contact_plugin_de_activation' );
+
+
+/**
+ * Enable WordPress cron for specific modules.
+ *
+ * @param string     $module The module to load the cron for. If it's not active then quit.
+ * @param string|int $cron_duration The duration of the cron job.
+ * @return void
+ */
+function toolbelt_enable_cron( $module, $cron_duration = 'daily' ) {
+
+	$options = toolbelt_get_options();
+	$cron_key = 'toolbelt_cron_' . $cron_duration;
+
+	// Quit if the module is not active.
+	if ( ! isset( $options[ $module ] ) || 'on' !== $options[ $module ] ) {
+		return;
+	}
+
+	// Enable the cron job if it hasn't been set up already.
+	if ( ! wp_next_scheduled( $cron_key ) ) {
+		wp_schedule_event( time(), $cron_duration, $cron_key );
+	}
+
+}
+
+
+/**
+ * Deactivate Toolbelt cron jobs.
+ *
+ * @return void
+ */
+function toolbelt_deactivate_cron() {
+
+	wp_clear_scheduled_hook( 'toolbelt_cron_daily' );
+	wp_clear_scheduled_hook( 'toolbelt_cron_weekly' );
+
+}
+
+add_filter( 'deactivate_wp-toolbelt', 'toolbelt_deactivate_cron' );
