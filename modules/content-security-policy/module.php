19c1fc7719de13881f113dd9342a1d55d01ca50a
Ben Gillbanks <binarymoon@gmail.com>
2020-07-27 19:37:53 +0100

    Update CSP

--- modules/content-security-policy/module.php
+++ modules/content-security-policy/module.php
@@ -23,78 +23,66 @@ function toolbelt_csp_headers() {
 		),
 		'upgrade-insecure-requests' => true,
 		'block-all-mixed-content'   => true,
-		'report-only'               => true,
+		'style-src'                 => array( '*', "'nonce-" . esc_attr( TOOLBELT_NONCE ) . "'" ),
+		'script-src'                 => array( '*', "'nonce-" . esc_attr( TOOLBELT_NONCE ) . "'" ),
+		'report-only'               => false,
 		'report-uri'                => '',
 		'report-to'                 => '', // New for CSP level 3 https://www.w3.org/TR/CSP/#changes-from-level-2.
 	);
 
 	$admin_policy = apply_filters( 'toolbelt_csp_policy', $default_options );
 
-	$_csp_cache = wp_cache_get( 'toolbelt-csp-cache', 'toolbelt' );
+	if ( isset( $admin_policy['report-only'] ) && true === $admin_policy['report-only'] ) {
 
-	// Only rebuild if cache is empty.
-	if ( false === $_csp_cache ) {
-
-		if ( true === $admin_policy['report-only'] ) {
-			// Testing mode.
-			$csp_string = 'Content-Security-Policy-Report-Only: ';
-		} else {
-			// Enforce mode.
-			$csp_string = 'Content-Security-Policy: ';
-		}
-
-		foreach ( $admin_policy as $rule => $setting ) {
-
-			if ( is_array( $setting ) ) {
-
-				/**
-				 * If we have a more complex rule, or one with multiple
-				 * properties, build it a piece at a time.
-				 */
-				$csp_string .= $rule;
-				foreach ( $setting as $option ) {
-
-					// Add the property to the rule.
-					$csp_string .= " {$option}";
-
-				}
-
-			} elseif ( true === $setting ) {
-
-				// Boolean values don't have any properties.
-				$csp_string .= "${rule}";
-
-			} else {
-
-				// Simpler k=>v properties.
-				$csp_string .= "{$rule} {$setting}";
-
-			}
-
-			// Separator.
-			$csp_string .= '; ';
-
-		}
-
-		$csp_string = trim( $csp_string );
-
-		// Stuff it into the cache.
-		wp_cache_set(
-			'toolbelt-csp-cache',
-			$csp_string,
-			'toolbelt',
-			HOUR_IN_SECONDS
-		);
-
-		// Send the header.
-		header( $csp_string );
+		// Testing mode.
+		$csp_string = 'Content-Security-Policy-Report-Only: ';
 
 	} else {
 
-		// Send the cached header.
-		header( $_csp_cache );
+		// Enforce mode.
+		$csp_string = 'Content-Security-Policy: ';
 
 	}
+
+	foreach ( $admin_policy as $rule => $setting ) {
+
+		if ( is_array( $setting ) ) {
+
+			/**
+			 * If we have a more complex rule, or one with multiple
+			 * properties, build it a piece at a time.
+			 */
+			$csp_string .= $rule;
+			foreach ( $setting as $option ) {
+
+				// Add the property to the rule.
+				$csp_string .= " {$option}";
+
+			}
+
+		} elseif ( true === $setting ) {
+
+			// Boolean values don't have any properties.
+			$csp_string .= "${rule}";
+
+		} else {
+
+			// Simpler k=>v properties.
+			$csp_string .= "{$rule} {$setting}";
+
+		}
+
+		// Separator.
+		$csp_string .= '; ';
+
+	}
+
+	$csp_string = trim( $csp_string );
+
+	// var_dump( $csp_string );
+
+	// Send the header.
+	header( $csp_string );
 
 }
 
