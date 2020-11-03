7ead3dfa78753e646c50246cf6280931ef1bf9b9
Ben Gillbanks <binarymoon@gmail.com>
2020-11-03 21:30:40 +0000

    Update admin-credits.php

--- admin/admin-credits.php
+++ admin/admin-credits.php
@@ -15,9 +15,19 @@
 		border-radius: 5px;
 	}
 	.toolbelt-ratings p { font-size: 14px; }
+	.toolbelt-links [role=separator] {
+		margin: 0 0.5em;
+		opacity: 0.5;
+	}
 </style>
 
 <div class="toolbelt-ratings">
 	<p><?php esc_html_e( 'Toolbelt is a free plugin. If you find it useful then a review on wordpress.org would be most appreciated!', 'wp-toolbelt' ); ?></p>
 	<p><a href="https://wordpress.org/support/plugin/wp-toolbelt/reviews/"><?php esc_html_e( 'Leave a review &rsaquo;', 'wp-toolbelt' ); ?></a></p>
 </div>
+
+<nav class="toolbelt-links">
+	<a href="https://github.com/BinaryMoon/wp-toolbelt"><?php esc_html_e( 'Join us on Github', 'wp-toolbelt' ); ?></a>
+	<span role="separator">|</span>
+	<a href="https://github.com/BinaryMoon/wp-toolbelt/wiki"><?php esc_html_e( 'Read the docs', 'wp-toolbelt' ); ?></a>
+</nav>
