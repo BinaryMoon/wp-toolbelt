89fff183ca7842e571a5c9a1fd9e53c7645975b9
Ben Gillbanks <binarymoon@gmail.com>
2021-06-13 15:26:43 +0100

    Update jetpack dev mode disable

--- modules/jetpack-dev-mode/module.php
+++ modules/jetpack-dev-mode/module.php
@@ -5,4 +5,5 @@
  * @package toolbelt
  */
 
-add_filter( 'jetpack_development_mode', '__return_true' );
+// add_filter( 'jetpack_development_mode', '__return_true' );
+add_filter( 'jetpack_offline_mode', '__return_true' );
